7843e099ec6d51de150dc0a1630fb34ce7698bc2
nnhu <1337285102@qq.com>
2021-12-27 19:05:25 +0800

    transition 错误

--- src/nh-design/package/transition/transition.vue
+++ src/nh-design/package/transition/transition.vue
@@ -12,41 +12,58 @@
 </template>
 
 <script>
-/* 
+/*
  * 这里不提供 over: hidden
  * 这里不提供 transition: .3s
  */
 export default {
   name: "nhTransition",
+  data() {
+    return {
+      scrollHeight: "",
+    };
+  },
   methods: {
     beforeEnter(el) {
-      el.style.height = 0;
+      el.dataset.oldPaddingTop = el.style.paddingTop;
+      el.dataset.oldPaddingBottom = el.style.paddingBottom;
+      el.style.paddingTop = 0;
+      el.style.paddingBottom = 0;
+
+      el.style.height = "0px";
       el.style.opacity = 0;
     },
+
     enter(el) {
-      if (el.scrollHeight != 0) {
-        el.style.height = el.scrollHeight + "px";
-      } else {
-        el.style.height = "";
-      }
-      el.style.opacity = 1;
-    },
-    afterEnter(el) {
-      el.style.height = "";
-    },
-    beforeLeave(el) {
+      this.scrollHeight = el.scrollHeight;
       el.style.height = el.scrollHeight + "px";
+
+      el.style.paddingTop = el.dataset.oldPaddingTop;
+      el.style.paddingBottom = el.dataset.oldPaddingBottom;
       el.style.opacity = 1;
+      el.style.overflow = "hidden";
+    },
+    afterEnter() {
+      // el.style.height = "";
+    },
+
+    beforeLeave(el) {
+      el.style.opacity = 1;
+      this.$nextTick(() => {
+        console.log(el.scrollHeight);
+        // el.style.height = this.scrollHeight + "px";
+        el.style.height = el.scrollHeight + "px";
+      });
     },
     leave(el) {
-      if (el.scrollHeight !== 0) {
-        el.style.height = "0px";
-      }
-      el.style.opacity = 0;
+      el.style.height = "0px";
+      el.style.overflow = "hidden";
+      el.style.paddingTop = 0;
+      el.style.paddingBottom = 0;
     },
     afterLeave(el) {
-      el.style.height = "0px";
       el.style.height = "";
+      el.style.opacity = 0;
     },
   },
 };
